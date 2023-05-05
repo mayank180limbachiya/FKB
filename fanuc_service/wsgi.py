9cb05cf99fa0c1f90bb404b76d5d3087d9d7459e
Mayank Jitendrakumar Limbachiya <mayank.limbachiya@fanucindia.com>
2023-05-05 15:23:02 +0530

    Without header

--- fanuc_service/wsgi.py
+++ fanuc_service/wsgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 WSGI config for fanuc_service project.
 
@@ -15,21 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fanuc_service.settings')
 
 application = get_wsgi_application()
-=======
-"""
-WSGI config for fanuc_service project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fanuc_service.settings')
-
-application = get_wsgi_application()
->>>>>>> master
--- servicesupport/apps.py
+++ servicesupport/apps.py
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-from django.apps import AppConfig
-
-
-
-class ServicesupportConfig(AppConfig):
-    name = 'servicesupport'
-=======
 from django.apps import AppConfig
 
 
 class ServicesupportConfig(AppConfig):
     name = "servicesupport"
->>>>>>> master
